05a1b637e8a70f7f4c5395512945301a75b569d7
RamonvCS <ramonvalentincs@gmail.com>
2025-05-09 12:14:16 -0400

    Updated launch data and HTML templates

--- services/api_client.py
+++ services/api_client.py
@@ -2,17 +2,11 @@ import requests
 
 def get_upcoming_launches():
     url = 'https://ll.thespacedevs.com/2.2.0/launch/upcoming/'
-<<<<<<< HEAD
-    response = requests.get(url)
-    data = response.json()
-    return data['results']
-=======
     try:
         response = requests.get(url)
         response.raise_for_status()  # Lanza error si el status code es 4xx o 5xx
         data = response.json()
-        return data.get('results', [])  # Devuelve [] si no hay 'results'
+        return data.get('results', [])  # Devuelve [] si 'results' no está presente
     except Exception as e:
         print(f"❌ Error al obtener datos de lanzamientos: {e}")
         return []
->>>>>>> dev
